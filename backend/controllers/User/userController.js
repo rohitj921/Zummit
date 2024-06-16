e83700c3e8f00040d4df7a85d5bfc9e3ef0c8f77
Dishant Nishad <97354577+itsdishantnishad@users.noreply.github.com>
2024-06-16 17:39:03 +0530

    Created API for upcoming Groups (#117)
    * Appointment list controller
    
    * adding upcoming group api
    
    * UpComming Groups QA Fixed

--- backend/controllers/User/userController.js
+++ backend/controllers/User/userController.js
@@ -2,6 +2,7 @@ const asyncHandler = require("express-async-handler");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const User = require("../../models/User/userModel");
+const UpcomingGroups = require("../../models/User/upcomingGroups");
 
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -9,42 +10,42 @@ const generateToken = (id) => {
   });
 };
 
-const registerUser = asyncHandler(async (req, res) => {  // control variable hai bhai
+const registerUser = asyncHandler(async (req, res) => {
+  // control variable hai bhai
   let isValid = false;
-  let msg = '';
-  let email='';
-  let phone='';
+  let msg = "";
+  let email = "";
+  let phone = "";
 
-  const { name, input, password,role } = req.body;
+  const { name, input, password, role } = req.body;
 
-  console.log( name, input, password,role);
+  console.log(name, input, password, role);
   if (password.length < 6) {
     res.status(400);
     throw new Error("password must be upto 6 character");
   }
 
-   
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (emailRegex.test(input)) {
-        email=input;
-        isValid = true;
-        console.log(email)
-        msg = 'Valid email address.';
-    }
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (emailRegex.test(input)) {
+    email = input;
+    isValid = true;
+    console.log(email);
+    msg = "Valid email address.";
+  }
 
-    
-    const phoneRegex = /^\+?(\d{1,3})?[-. (]?\d{3}[-. )]?\d{3}[-. ]?\d{4}$/;
-    if (phoneRegex.test(input)) {
-        phone=input;
-        isValid = true;
-        console.log(phone)
-        msg = 'Valid phone number.';
-    }
+  const phoneRegex = /^\+?(\d{1,3})?[-. (]?\d{3}[-. )]?\d{3}[-. ]?\d{4}$/;
+  if (phoneRegex.test(input)) {
+    phone = input;
+    isValid = true;
+    console.log(phone);
+    msg = "Valid phone number.";
+  }
 
-    
-    if (!isValid) {
-        return res.status(400).json({ success: false, msg: 'Invalid email or phone number.' });
-    }
+  if (!isValid) {
+    return res
+      .status(400)
+      .json({ success: false, msg: "Invalid email or phone number." });
+  }
 
   const User = require("../../models/User/userModel");
 
@@ -61,20 +62,19 @@ const registerUser = asyncHandler(async (req, res) => {  // control variable hai
     password,
   });
 
-    
   if (user) {
-    const { _id, input,role } = user;
-    console.log("ID is ",_id)
+    const { _id, input, role } = user;
+    console.log("ID is ", _id);
     const token = generateToken(_id);
     res.cookie("token", token, {
       path: "/",
       httpOnly: true,
       expires: new Date(Date.now() + 1000 * 86400),
-        //  secure:true,  //for deployment
-        //  sameSite:none
+      //  secure:true,  //for deployment
+      //  sameSite:none
     });
     // send user data
-        res.status(201).json({
+    res.status(201).json({
       success: true,
       _id,
       name,
@@ -84,73 +84,80 @@ const registerUser = asyncHandler(async (req, res) => {  // control variable hai
       message: "regisered successfully",
     });
   } else {
-    res.status(400).json({error:"Invalid user Data"});
+    res.status(400).json({ error: "Invalid user Data" });
   }
-})
+});
 
 const loginUser = async (req, res) => {
-  try{
-    
-  const { input, password } = req.body;
+  try {
+    const { input, password } = req.body;
 
-  if (!input || !password) {
-    res.status(400);
-    throw new Error("please add email/phone & password");
-  }
+    if (!input || !password) {
+      res.status(400);
+      throw new Error("please add email/phone & password");
+    }
 
-  const user = await User.findOne({ input });
-  
-  const passwordIsCorrect = await bcrypt.compare(password, user.password);
+    const user = await User.findOne({ input });
 
-  if(!user){
-    res.status(400)
-    throw new Error("user does not exist")
-  }
-  const token = generateToken(user._id);
-  if (user && passwordIsCorrect) {
-    const newUser=await User.findOne({input}).select("-password")
-    res.cookie("token",token,{
-        path:"/",
-        httpOnly:true,
-        expires: new Date(Date.now()+1000*86400),
+    const passwordIsCorrect = await bcrypt.compare(password, user.password);
+
+    if (!user) {
+      res.status(400);
+      throw new Error("user does not exist");
+    }
+    const token = generateToken(user._id);
+    if (user && passwordIsCorrect) {
+      const newUser = await User.findOne({ input }).select("-password");
+      res.cookie("token", token, {
+        path: "/",
+        httpOnly: true,
+        expires: new Date(Date.now() + 1000 * 86400),
         // secure:true,
         // sameSite:none,
-    });
+      });
 
-    res.status(201).json({newUser,"Authorization":token})
-  }else{
-    res.status(400);
-    throw new Error("Invalid email or Password");
-  }
-  }
-  catch(error){
+      res.status(201).json({ newUser, Authorization: token });
+    } else {
+      res.status(400);
+      throw new Error("Invalid email or Password");
+    }
+  } catch (error) {
     console.log(error);
   }
-  
 };
 
+const logout = asyncHandler(async (req, res) => {
+  res.clearCookie("token");
+  res.status(200).json({
+    message: "successfully logout",
+  });
+});
 
-const logout=asyncHandler(async(req,res)=>{
-    res.clearCookie('token');
+const getUser = (req, res) => {
+  if (token) {
+    const user = User.findBy(body.input);
     res.status(200).json({
-        message:"successfully logout"
-    })
-})
+      success: true,
+      data: user,
+      msg: "user found",
+    });
+  }
+};
 
-const getUser=(req,res)=>{
-    if(token){
-      const user=User.findBy(body.input);
-      res.status(200).json({
-        success:true,
-        data:user,
-        msg:"user found"
-      })
-    }
-}
+const upcomingGroups = asyncHandler(async (req, res)=> {
+  try {
+    const groups = await UpcomingGroups.find();
+    res.status(200).json(groups);
+  } catch (err) {
+    console.error("Error fetching upcoming groups", err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
 
 module.exports = {
   registerUser,
   loginUser,
   logout,
-  getUser
+  getUser,
+  upcomingGroups,
 };
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,6 +1,6 @@
 const express=require("express")
 const router=express.Router();
-const {registerUser, loginUser, logout, getUser}=require("../controllers/User/userController");
+const {registerUser, loginUser, logout, getUser,upcomingGroups}=require("../controllers/User/userController");
 const { protect } = require("../middleware/authMiddleware");
 const { getGroupTherapySessions, createGroupTherapySession } = require("../controllers/User/groupTherapyController");
 const { createSupportGroupSession, getSupportGroupSessions } = require("../controllers/User/supportgroupController");
@@ -12,9 +12,10 @@ router.get("/Supportgroup", getSupportGroupSessions);
 router.post("/Supportgroup", createSupportGroupSession);
 
 
+
 router.post("/register",registerUser);
 router.post("/login",loginUser)
 router.get("/logout",logout)
 router.get("/getUser",protect,getUser)
-
+router.get("/upcomingGroups",upcomingGroups);
 module.exports=router;
