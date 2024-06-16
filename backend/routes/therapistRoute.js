90c9ba8387e8a0f50c753fe9d657bf1a91b3d9c1
nishaa12 <nishakrim40@gmail.com>
2024-06-16 12:38:14 +0530

    created route for therapist group

--- backend/routes/therapistRoute.js
+++ backend/routes/therapistRoute.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const { loginTherapist, logoutTherapist, getTherapist } = require("../controllers/Admin/therapistCredentials/createCredentials");
 const { therapistAppointmentLists, createtherapistAppointmentLists } = require("../controllers/Therapist/therapistAppointmentsController.js")
+const{therapistGroupLists,createtherapistGroupLists}=require("../controllers/Therapist/therapistGroupsController.js")
 
 router.route("/loginTherapist").post(loginTherapist);
 router.route("/logoutTherapist").get(logoutTherapist);
@@ -10,6 +11,8 @@ router.route("/getTherapist").get(getTherapist);
 
 router.route("/createtherapistAppointment").post(createtherapistAppointmentLists);
 router.route("/gettherapistAppointmentLists").get(therapistAppointmentLists);
+router.route("/createtherapistGroup").post(createtherapistGroupLists);
+router.route("/gettherapistGroupLists").get(therapistGroupLists);
 
 
 
