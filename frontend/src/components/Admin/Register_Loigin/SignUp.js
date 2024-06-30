0394febee70c968af2bcedf10d73608ec41b3b4c
Kuldeep Saini <kuldeepsaini5514@gmail.com>
2024-06-30 19:19:41 +0530

    Dependancy removed

--- frontend/src/components/Admin/Register_Loigin/SignUp.js
+++ frontend/src/components/Admin/Register_Loigin/SignUp.js
@@ -3,11 +3,7 @@ import { addUser } from "../../../utils/userSlice";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
-import IconButton from "@material-ui/core/IconButton";
-import Visibility from "@material-ui/icons/Visibility";
-import InputAdornment from "@material-ui/core/InputAdornment";
-import VisibilityOff from "@material-ui/icons/VisibilityOff";
-import Input from "@material-ui/core/Input";
+
 //main component toh yaha hey
 const SignUp = () => {
   const dispatch = useDispatch();
