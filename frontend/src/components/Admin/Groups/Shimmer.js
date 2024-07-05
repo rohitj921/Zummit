96bc03669adee0ac41df5b50b47ef49055d09cd9
Kuldeep Saini <kuldeepsaini5514@gmail.com>
2024-07-05 21:01:37 +0530

    [FE]: Admin Group Shimmer Effect

---
+++ frontend/src/components/Admin/Groups/Shimmer.js
@@ -0,0 +1,48 @@
+import React from "react";
+
+const Shimmer = () => {
+  return (
+    <div className="w-full m-5">
+      {/* search bar */}
+      <div className="w-[90%] ml-20 flex justify-around items-center">
+        <a className="relative w-[70%] h-12 block overflow-hidden">
+          <span id="loader-element"></span>
+        </a>
+        <a className="relative w-12 rounded-full h-12 block overflow-hidden">
+          <span id="loader-element"></span>
+        </a>
+      </div>
+
+      <div className="w-[90%] flex justify-between mt-10 mx-5">
+        <a className="relative w-[25%] h-12 block overflow-hidden">
+          <span id="loader-element"></span>
+        </a>
+        <a className="relative w-[15%] h-12 block overflow-hidden">
+          <span id="loader-element"></span>
+        </a>
+      </div>
+
+      {/* Array list */}
+      <div className="w-[90%] mt-10 flex flex-wrap gap-10 mx-5 rounded-md p-5" id="shadow">
+        {Array(6)
+          .fill("")
+          .map(() => (
+            <div className="w-[30%]">
+              <a className="relative w-[100%] h-60 block overflow-hidden">
+                <span id="loader-element"></span>
+              </a>
+              <div className="mt-2 px-2">
+              <a className="relative w-[100%] mb-2 h-10 block overflow-hidden">
+                <span id="loader-element"></span>
+              </a>   <a className="relative w-[100%] h-16 block overflow-hidden">
+                <span id="loader-element"></span>
+              </a> 
+              </div>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default Shimmer;
