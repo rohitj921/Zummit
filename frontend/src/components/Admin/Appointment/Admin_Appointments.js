260d7d065c2fff3c381b1e756a02fe15aeb8d558
Kuldeep Saini <kuldeepsaini5514@gmail.com>
2024-07-05 20:59:03 +0530

    [FE]: Admin Appointmnet Shimmer Effect

--- frontend/src/components/Admin/Appointment/Admin_Appointments.js
+++ frontend/src/components/Admin/Appointment/Admin_Appointments.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Admin_SideBar from "../Admin_SideBar";
 import Appointment from "./Appointment";
+import Shimmer from "./Shimmer";
 
 const Admin_Appointments = () => {
+  const [toggleShimmer, setToggleShimmer] = useState(true)
+  useEffect(()=>{
+    setInterval(() => {
+      setToggleShimmer(false)
+    }, 2000);
+  },[])
   return (
     <div className="flex bg-[#F2FCFF]">
       <Admin_SideBar/>
-      <Appointment/>
+      {toggleShimmer ?<Shimmer/> : <Appointment/>}
+      
     </div>
   );
 };
